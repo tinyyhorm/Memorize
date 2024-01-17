0ee087d93666de1c6409e99793876563d5bc20ac
tiny-mba <zzuli.zgh@gmail.com>
2024-01-17 12:52:27 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by ZhouGuohao on 17/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
