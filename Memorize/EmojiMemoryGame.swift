a1cd302e7dc888edeff398ec66b983f6be019ad6
tiny-mba <zzuli.zgh@gmail.com>
2024-01-22 20:33:25 +0800

    Lecture 4 | Stanford CS193p 2023 | Part 1, 00:31:24

--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -5,4 +5,20 @@
 //  Created by ZhouGuohao on 19/01/24.
 //
 
-import Foundation
+import SwiftUI
+
+class EmojiMemoryGame {
+    let emoji = ["🤓", "🥲", "🤓", "🥲", "🤓", "🥲", "🤓", "🥲"]
+
+    private var model = MemoryGame(numberOfPairsOfCards: 4) {index in
+        return emoji[index]
+    }
+    
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
+}
--- Memorize/MemoryGame.swift
+++ Memorize/MemoryGame.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+struct MemoryGame<CardContent> {
+    private (set) var cards: Array<Card>
+    
+    init(numberOfPairsOfCards: Int, cardContentFactory: (Int) -> CardContent) {
+        cards = []
+        // add numberOfPairsOfCards x 2 cards
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = cardContentFactory(pairIndex)
+            cards.append(Card(content: content))
+            cards.append(Card(content: content))
+        }
+    }
+    
+    func choose(_ card: Card) {
+        
+    }
+    
+    struct Card {
+        var isFaceUp = false
+        var isMatched = false
+        let content: CardContent
+    }
+}
